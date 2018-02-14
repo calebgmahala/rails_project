7d35afa5804f57481cf86bac5cdd467138e5d03b
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-02-14 15:09:04 -0500

    added users

---
+++ db/migrate/20180214195026_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :team
+      t.string :password
+      t.integer :permission
+
+      t.timestamps
+    end
+  end
+end
