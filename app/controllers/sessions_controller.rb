8fb36eac6b1edc94b4b94b20d3e2f5240d511f8c
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-02-16 14:00:42 -0500

    hope this is ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(username: params[:session][:username].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		redirect_to @user
+  	else
+  		fail RuntimeError, "implemement failed login"
+  	end
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :events
   resources :users
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
