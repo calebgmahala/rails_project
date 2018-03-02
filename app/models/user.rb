f6ecaf9598c390f3741ae9eed4f66d0b668e45bd
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-03-02 11:20:56 -0500

    next fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
 	validates :username, presence: true
-	validates :password, presence: true
 	validates :team, presence: true
 	validates :permission, presence: true
+
+	after_initialize :set_permission
+
+	def authenticate(provided_password)
+		self.password == provided_password
+	end
+	def set_permission
+		self.permission ||= 2
+	end
 	has_secure_password
 end
